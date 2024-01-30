68d5e33d47637415fb616e039a6ee17a97f270ad
TMNG <ktq2390325@stu.o-hra.ac.jp>
2024-01-30 09:24:35 +0900

    Commit

---
+++ M15/LastTime1.java
@@ -0,0 +1,51 @@
+package M15;
+import java.io.*;
+import java.util.Scanner;
+
+class LastTime1 {
+    static void init() {
+        BufferedReader br =null;
+        try {
+            br = new BufferedReader(new FileReader("LastTime.tet"));
+            String kibun = br.readLine();
+            System.out.println("前回の気分は" + kibun + "でした。");
+        }catch (IOException e){
+            System.out.println("このプログラムを実行するのは初めてです。");
+        }finally{
+            if(br != null) {
+                try {
+                    br.close();
+                }catch (IOException e){
+                    System.out.println("ファイルクローズ失敗。");
+                }
+            }
+        }
+    }
+
+    static void term(String kibun) {
+        FileWriter fw = null;
+
+        try{
+            fw = new FileWriter("LastTime.txt");
+            fw.write(kibun);
+        } catch (IOException e) {
+            System.out.println("エラー発生!!");
+        }finally {
+            if (fw != null) {
+                try {
+                    fw.close();
+                }catch (IOException e){
+                    System.out.println("ファイルクローズ失敗。");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        init();
+        System.out.print("今の気分は:");
+        String kibun = stdIn.next();
+        term(kibun);
+    }
+}
