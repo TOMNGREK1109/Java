5405c020569db56d166f3b346ed04eb814d65b47
TMNG <ktq2390325@stu.o-hra.ac.jp>
2024-02-05 11:51:57 +0900

    Commit

--- kadaifail/crass/main.java
+++ kadaifail/crass/main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class main {
     public void name(String[] args){
         Scanner stdIn = new Scanner(System.in);
-        System.out.print("月を入力："); String s1 = stdIn.next();
+        System.out.print("月を入力："); String x = stdIn.next();
     }
 }
