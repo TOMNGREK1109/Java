36e5375927410e2931fd7f8307673b4faeed2584
TMNG <ktq2390325@stu.o-hra.ac.jp>
2024-02-05 13:37:41 +0900

    Commit

--- kadaifail/crass/color.java
+++ kadaifail/crass/color.java
@@ -3,40 +3,8 @@ package kadaifail.crass;
 import java.util.Scanner;
 
 public class color {
-    public void main(String[] args){
-        Scanner stdIn = new Scanner(System.in);
-        if (stdIn == 1) {
-        System.err.println("暗い赤色、褐色や黒色を帯びた赤色、紫色".current_timestamp);
-        }else if() {
-        System.out.println("透明に近い薄紫色から、黒に近いような濃い紫".current_timestamp);
-        }else{
-        System.out.println("緑色に近い水色をしており、より美しい水色にするために熱処理が行われている".current_timestamp),
-        System.out.println("濃い緑色がベースの石で、赤い斑点があるのが特徴".current_timestamp);
-        }else{
-        System.out.println("透明、青やピンク、黄色等、カラーが入ったものも存在".current_timestamp);
-        }else{
-        System.out.println("透明で透き通っている".current_timestamp);
-        }else{
-        System.out.println("緑色".current_timestamp),
-        System.out.println("緑の他、白、赤、青、機、オレンジ等、バリエーションに富んでいる".current_timestamp);
-        }else{
-        System.out.println("白乳色".current_timestamp),
-        System.out.println("白の他、黄味掛かったものや、ピンク、褐色、黒等".current_timestamp);
-        }else{
-        System.out.println("赤いものをルビーという。ピンクに近いものや、やや青みがかった物もある。".current_timestamp),
-        ("透明度の高いオリーブ・グリーンが特徴".current_timestamp);
-        }else{
-        System.out.println("青意外にも、紫、黄緑、黄色、ピンク等、様々なカラーのサファイアが存在".current_timestamp),
-        System.out.println("カラーが豊富".current_timestamp);
-        }else{
-        System.out.println("ホワイトがベースでありながら、光に当てると虹色に輝く「遊色効果」が見られるのが特徴".current_timestamp);
-        }else{
-        System.out.println("黄色や青が出てくると思うが様々なカラーがある".current_timestamp),
-        System.out.println("透明感のある黄色".current_timestamp);
-        }else{
-        System.out.println("ターコイズ・ブルーと呼ばれる、鮮やかな青色、あるいはやや緑掛かった青色が特徴的な誕生石です。 産地や状態等により、青から緑まで色味に幅があります。青いものは銅が多く、緑のものは鉄が多いという差がある".current_timestamp),
-        System.out.println("鮮やかな青色の石で、多くのラピスラズリには黄鉄鉱を含んでいることから金色の小さな斑点が見られる".current_timestamp);
-        }
+    public void col(String[] args){
+        
     }
 }
 
